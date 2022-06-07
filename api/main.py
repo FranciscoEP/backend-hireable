173766f838583168acf38a738f31c9e04a9ef5f9
FranciscoEP <francisco.eugp@gmail.com>
2022-06-06 22:05:51 -0500

    minor changes

--- api/main.py
+++ api/main.py
@@ -10,14 +10,7 @@ import uvicorn
 app = FastAPI()
 
 origins = [
-    
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
     settings.BASE_URL
-
 ]
 
 app.add_middleware(
